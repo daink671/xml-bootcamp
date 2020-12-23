cdd9e4a81dd208e8ebd7817e0580d118a798365a
daink671 <dainkim.974@gmail.com>
2020-12-23 16:55:05 +0000

    working

--- views/js/table.js
+++ views/js/table.js
@@ -1,63 +1,47 @@
-function draw_table()
-{
-	$("#results").empty();
-	$.getJSONuncached = function (url)
-	{
-		return $.ajax(
-		{
-			url: url,
-			type: 'GET',
-			cache: false,
-			success: function (html)
-			{
-				$("#results").append(html);
-				select_row();
-			}
-		});
-	};
-	$.getJSONuncached("/get/html")
-<<<<<<< HEAD
+function draw_table() {
+    $("#results").empty();
+    $.getJSONuncached = function (url) {
+        return $.ajax(
+            {
+                url: url,
+                type: 'GET',
+                cache: false,
+                success: function (html) {
+                    $("#results").append(html);
+                    select_row();
+                }
+            });
+    };
+    $.getJSONuncached("/get/html")
 };
 
-function select_row()
-=======
-};function select_row()
->>>>>>> 78e6dc0c025928a5720ca9ed67a7f3f30c9f3add
-{
-	$("#menuTable tbody tr[id]").click(function ()
-	{
-		$(".selected").removeClass("selected");
-		$(this).addClass("selected");
-		var section = $(this).prevAll("tr").children("td[colspan='3']").length - 1;
-		var entree = $(this).attr("id") - 1;
-		delete_row(section, entree);
-	})
+function select_row() {
+    $("#menuTable tbody tr[id]").click(function () {
+        $(".selected").removeClass("selected");
+        $(this).addClass("selected");
+        var section = $(this).prevAll("tr").children("td[colspan='3']").length - 1;
+        var entree = $(this).attr("id") - 1;
+        delete_row(section, entree);
+    })
 };
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 78e6dc0c025928a5720ca9ed67a7f3f30c9f3add
-function delete_row(sec, ent)
-{
-	$("#delete").click(function ()
-	{
-		$.ajax(
-		{
-			url: "/post/delete",
-			type: "POST",
-			data:
-			{
-				section: sec,
-				entree: ent
-			},
-			cache: false,
-			success: setTimeout(draw_table, 1000)
-		})
-	})
+function delete_row(sec, ent) {
+    $("#delete").click(function () {
+        $.ajax(
+            {
+                url: "/post/delete",
+                type: "POST",
+                data:
+                {
+                    section: sec,
+                    entree: ent
+                },
+                cache: false,
+                success: setTimeout(draw_table, 1000)
+            })
+    })
 };
 
-$(document).ready(function ()
-{
-	draw_table();
+$(document).ready(function () {
+    draw_table();
 });
